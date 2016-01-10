41d3af2f41b809654d6e5f8d0737190cee95c717
Edele Gizatullin <me@edele.ru>
2016-01-10 22:34:55 +0500

    artists & labels

---
+++ database/seeds/ArtistsSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Artist;
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+
+class ArtistsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Artist::create([
+            'name' => 'Muse',
+            'origin' => 'Teignmouth, Devon, England',
+            'activeFrom' => Carbon::create(1994),
+            'activeTo' => null,
+        ]);
+    }
+}
